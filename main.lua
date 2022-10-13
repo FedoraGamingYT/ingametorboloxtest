f536fa5e1d5224996eb745de80df77a5d62bea1c
Fedora <38592294+FedoraGamingYT@users.noreply.github.com>
2022-10-13 14:58:04 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
 -- https://pastebin.com/HiLgBvg2 - Update the version when a new update has been sent to the module.
+local OSPrimary = {}
+function OSPrimary.loadOS()
+  print("Loaded latest version of the os!")
+end
+return OSPrimary
